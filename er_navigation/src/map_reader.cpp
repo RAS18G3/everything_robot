70fc7363e78adb8304fd3828e94e93cd72b900fb
Leonard Bruns <leonardb@kth.se>
2018-10-06 18:25:36 +0200

    Add error handling to map_reader

--- er_navigation/src/map_reader.cpp
+++ er_navigation/src/map_reader.cpp
@@ -6,27 +6,33 @@ MapReader::MapReader(std::string path_to_map) {
   std::ifstream ifs;
   double xstart, xend, ystart, yend;
   std::string line;
-  ifs.open(path_to_map);
 
-  while(!ifs.eof()) {
-    std::getline(ifs, line);
+  try {
+    ifs.open(path_to_map);
+    while(!ifs.eof()) {
+      if (ifs.fail()) {
+        throw 1;
+      }
+      std::getline(ifs, line);
 
-    // skip comments
-    if(line[0] == '#' or ifs.eof())
-      continue;
+      // skip comments
+      if(line[0] == '#' or ifs.eof())
+        continue;
 
-    // read the coordinates of the wall in the predefined format
-    std::istringstream iss(line);
-    iss >> xstart >> ystart >> xend >> yend;
+      // read the coordinates of the wall in the predefined format
+      std::istringstream iss(line);
+      iss >> xstart >> ystart >> xend >> yend;
 
-    // store all these coordinates inside the walls vector
-    walls.emplace_back(xstart, xend, ystart, yend);
+      // store all these coordinates inside the walls vector
+      walls.emplace_back(xstart, xend, ystart, yend);
 
-    ROS_DEBUG_STREAM(xstart << " " << ystart << " " << xend << " " << yend);
+      ROS_DEBUG_STREAM(xstart << " " << ystart << " " << xend << " " << yend);
+    }
+    ROS_INFO_STREAM("Map succesfully read.");
   }
-
-
-  ROS_INFO_STREAM("Map succesfully read.");
+  catch (...) {
+    ROS_ERROR("Unknown error when reading the map, check the file path");
+  }
 }
 
 MapReader::~MapReader() {
@@ -34,5 +40,7 @@ MapReader::~MapReader() {
 }
 
 nav_msgs::OccupancyGrid MapReader::occupancy_grid() const {
-  
+  nav_msgs::OccupancyGrid occupancy_grid_msg = nav_msgs::OccupancyGrid();
+
+  return occupancy_grid_msg;
 }

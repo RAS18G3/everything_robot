809deeb5ab377ec9dd18609401981a120cd7fea6
Leonard Bruns <leonardb@kth.se>
2018-10-07 13:33:56 +0200

    Address PR comments and fix bug related to omega_avg== 0

--- er_wheels/src/er_wheel_odometry_node.cpp
+++ er_wheels/src/er_wheel_odometry_node.cpp
@@ -1,7 +1,7 @@
 #include "er_wheel_odometry_node.h"
 
 
-WheelOdometryNode::WheelOdometryNode() : ticks_per_rev_(3591.84/4), wheel_radius_(0.049), base_radius_(0.085), yaw_(0) {
+WheelOdometryNode::WheelOdometryNode() : ticks_per_rev_(3591.84/4), wheel_radius_(0.049), base_radius_(0.085), yaw_(0), loop_rate_(50) {
   angular_velocity_[MotorSide::Left] = 0;
   angular_velocity_[MotorSide::Right] = 0;
   // note messages are initialized with 0, so intiially the /base_frame will align with the /map frame
@@ -11,9 +11,9 @@ WheelOdometryNode::~WheelOdometryNode() {
 
 }
 
-void WheelOdometryNode::run_node() {
-  ros::NodeHandle n;
+void WheelOdometryNode::init_node() {
 
+  // read all parameteres
   std::string node_name = ros::this_node::getName();
   std::string left_motor_name, right_motor_name;
   bool left_inverted, right_inverted;
@@ -24,22 +24,24 @@ void WheelOdometryNode::run_node() {
   inverted_sign_[MotorSide::Left] = left_inverted ? -1 : 1;
   inverted_sign_[MotorSide::Right] = right_inverted ? -1 : 1;
 
-  ROS_INFO("inverted left %d right %d", inverted_sign_[MotorSide::Left], inverted_sign_[MotorSide::Right]);
+  ROS_DEBUG("inverted left %d right %d", inverted_sign_[MotorSide::Left], inverted_sign_[MotorSide::Right]);
 
   last_odometry_msg_.header.stamp = ros::Time::now();
   last_odometry_msg_.header.frame_id = "odom";
   last_odometry_msg_.child_frame_id = "base_frame";
   last_odometry_msg_.pose.pose.orientation.w = 1;
 
-  odometry_pub_ = n.advertise<nav_msgs::Odometry>(node_name, 1);
+  odometry_pub_ = nh_.advertise<nav_msgs::Odometry>(node_name, 1);
 
   std::function<void(const phidgets::motor_encoder::ConstPtr&)> left_cb = std::bind(&WheelOdometryNode::encoder_callback, this, MotorSide::Left, std::placeholders::_1);
   std::function<void(const phidgets::motor_encoder::ConstPtr&)> right_cb = std::bind(&WheelOdometryNode::encoder_callback, this, MotorSide::Right, std::placeholders::_1);
 
-  ros::Subscriber encoder_sub_left = n.subscribe<phidgets::motor_encoder>(left_motor_name + "/encoder", 1, left_cb);
-  ros::Subscriber encoder_sub_right = n.subscribe<phidgets::motor_encoder>(right_motor_name + "/encoder", 1, right_cb);
+  encoder_sub_left_ = nh_.subscribe<phidgets::motor_encoder>(left_motor_name + "/encoder", 1, left_cb);
+  encoder_sub_right_ = nh_.subscribe<phidgets::motor_encoder>(right_motor_name + "/encoder", 1, right_cb);
+}
 
-  ros::Rate loop_rate(50);
+void WheelOdometryNode::run_node() {
+  init_node();
 
   while (ros::ok())
   {
@@ -49,7 +51,7 @@ void WheelOdometryNode::run_node() {
     // publish transform (this has to be done even if the callback is not called, so that the transform stays up-to-date)
     publish_transform();
 
-    loop_rate.sleep();
+    loop_rate_.sleep();
 
   }
 }
@@ -83,8 +85,10 @@ void WheelOdometryNode::update_odometry() {
   const double dt = (odometry_msg.header.stamp - last_odometry_msg_.header.stamp).toSec();
 
   // since we dont know when exactly between the last and current the velocity has changed, we take the average of the two measurements
+  // because of this it is important, that we actually publish messages frequently, even if we are not moving at all
+  // otherwise we would suddenly jump because of the large dt
   const double v_avg = (odometry_msg.twist.twist.linear.x + last_odometry_msg_.twist.twist.linear.x) / 2;
-  const double omega_avg = (odometry_msg.twist.twist.angular.z + last_odometry_msg_.twist.twist.angular.z) / 2;
+  double omega_avg = (odometry_msg.twist.twist.angular.z + last_odometry_msg_.twist.twist.angular.z) / 2;
   const double delta_omega = omega_avg * dt;
 
   // TODO: any nicer way of doing that, using the old message is super annoying because of quaternions...?
@@ -97,25 +101,30 @@ void WheelOdometryNode::update_odometry() {
   odometry_msg.pose.pose.orientation = tf2::toMsg(new_rotation);
 
   // find radius and center of the circle trajectory that the robot has been following the past dt
-  // TODO: different formulas when omega_avg is very small -> infinite circle / driving in a straight line
-  if(omega_avg == 0)
-    return;
-  const double r = v_avg / omega_avg; // TODO: check for omega_avg == 0? required?
-  const double icc_x = last_odometry_msg_.pose.pose.position.x - r * sin(yaw_);
-  const double icc_y = last_odometry_msg_.pose.pose.position.y + r * cos(yaw_);
-  const double delta_x = last_odometry_msg_.pose.pose.position.x - icc_x;
-  const double delta_y = last_odometry_msg_.pose.pose.position.y - icc_y;
+  // different formulas when omega_avg is very small to prevent numerical issues -> infinite circle / driving in a straight line
+  if(omega_avg <= 1e-6) {
+    odometry_msg.pose.pose.position.x = last_odometry_msg_.pose.pose.position.x + cos(yaw_) * v_avg * dt;
+    odometry_msg.pose.pose.position.y = last_odometry_msg_.pose.pose.position.y + sin(yaw_) * v_avg * dt;
+  }
+  else {
+    const double r = v_avg / omega_avg; // TODO: check for omega_avg == 0? required?
+    const double icc_x = last_odometry_msg_.pose.pose.position.x - r * sin(yaw_);
+    const double icc_y = last_odometry_msg_.pose.pose.position.y + r * cos(yaw_);
+    const double delta_x = last_odometry_msg_.pose.pose.position.x - icc_x;
+    const double delta_y = last_odometry_msg_.pose.pose.position.y - icc_y;
 
-  ROS_DEBUG("delta_omega: %f", delta_omega);
-  ROS_DEBUG("delta_x: %f", delta_x);
-  ROS_DEBUG("delta_y: %f", delta_y);
-  ROS_DEBUG("r %f", r);
-  ROS_DEBUG("yaw %f", yaw_);
-  ROS_DEBUG("icc_x %f", icc_x);
-  ROS_DEBUG("icc_y %f", icc_y);
+    ROS_DEBUG("delta_omega: %f", delta_omega);
+    ROS_DEBUG("delta_x: %f", delta_x);
+    ROS_DEBUG("delta_y: %f", delta_y);
+    ROS_DEBUG("r %f", r);
+    ROS_DEBUG("yaw %f", yaw_);
+    ROS_DEBUG("icc_x %f", icc_x);
+    ROS_DEBUG("icc_y %f", icc_y);
 
-  odometry_msg.pose.pose.position.x = icc_x + cos(delta_omega) * delta_x - sin(delta_omega) * delta_y;
-  odometry_msg.pose.pose.position.y = icc_y + sin(delta_omega) * delta_x + cos(delta_omega) * delta_y;
+    odometry_msg.pose.pose.position.x = icc_x + cos(delta_omega) * delta_x - sin(delta_omega) * delta_y;
+    odometry_msg.pose.pose.position.y = icc_y + sin(delta_omega) * delta_x + cos(delta_omega) * delta_y;
+  }
+
 
   last_odometry_msg_ = odometry_msg;
   odometry_pub_.publish(odometry_msg);

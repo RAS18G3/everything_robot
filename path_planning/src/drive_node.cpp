8eb95734e43d6f904e2484f98eafcd6c808d7e3f
fanny <fantastiskafanny@gmail.com>
2018-10-13 18:12:23 +0200

    fix

--- path_planning/src/drive_node.cpp
+++ path_planning/src/drive_node.cpp
@@ -89,18 +89,18 @@ int main(int argc, char **argv)
         twist_msg.angular.z = 0;
       }
       else if(abs(angle_to_point-odom_w) < 0.3){
-        ROS_INFO("spinning %f", angle_to_point-odom);
+        ROS_INFO("spinning %f", angle_to_point-odom_w);
         twist_msg.linear.x = 0;
         twist_msg.linear.y = 0;
         twist_msg.angular.z = 0.2*(angle_to_point-odom_w);
       }
       else{
-        ROS_INFO("driving %f", distance_goal)
+        ROS_INFO("driving %f", distance_goal);
         float horizon = 0.3;
 
         twist_msg.linear.x = 0.2;
         twist_msg.linear.y = 0;
-        twist_msg.angular.z = horizon*(angle_to_point-odom);
+        twist_msg.angular.z = horizon*sin(angle_to_point-odom_w);
       }
     }
 

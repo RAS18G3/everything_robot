4fb2051816681eb4ec17b224963fac4f53dfab98
Leonard Bruns <leonardb@kth.se>
2018-11-01 18:13:42 +0100

    small changes

--- er_gripper/src/er_grab_controller.cpp
+++ er_gripper/src/er_grab_controller.cpp
@@ -8,17 +8,22 @@ double x;
 double y;
 double width;
 double height;
+bool object_seen;
 
 void object_box_callback(const std_msgs::UInt16MultiArray::ConstPtr bounding_box_msg){
   x = bounding_box_msg -> data[object_num+0];
   y = bounding_box_msg -> data[object_num+1];
   width = bounding_box_msg -> data[object_num+2];
   height = bounding_box_msg -> data[object_num+3];
+
+  object_seen = true;
 }
 
 int main(int argc, char **argv){
   object_num = 0;
+  object_seen = false;
 
+  ros::init(argc, argv, "er_grab_controller");
   ros::NodeHandle n;
 
   ros::Subscriber object_box_sub = n.subscribe("object_bounding_boxes", 10, object_box_callback);
@@ -29,7 +34,7 @@ int main(int argc, char **argv){
     geometry_msgs::Twist twist_msg;
 
     double object_point = x + (width/2);
-    double middle_point = 300;
+    double middle_point = 320;
     double sideways_treshold = 70;
     double height_treshold = 30;
 
@@ -39,7 +44,7 @@ int main(int argc, char **argv){
     }
     else if (std::abs(object_point-middle_point) < sideways_treshold){
       twist_msg.linear.x = 0.1;
-      twist_msg.angular.z = 0.1*(object_point-middle_point);
+      twist_msg.angular.z = 0; //0.1*(object_point-middle_point);
     }
     else{
       twist_msg.linear.x = 0;

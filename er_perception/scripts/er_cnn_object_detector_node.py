6603a7d5e89ea6d82f33accc7fca73884aa61e7e
Leonard Bruns <leonardb@kth.se>
2018-11-04 23:23:29 +0100

    Add train code, trained cnn and cnn object detector node

---
+++ er_perception/scripts/er_cnn_object_detector_node.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+#import sys
+import roslib
+import rospy
+import cv2
+from std_msgs.msg import UInt16MultiArray
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+from simple_object_detector import find_bounding_boxes
+import keras
+
+DEBUG = False
+
+LABELS = ['Yellow Ball', 'Yellow Cube', 'Green Cube', 'Green Cylinder', 'Green Hollow Cube', 'Orange Cross', 'Patric', 'Red Cylinder', 'Red Hollow Cube', 'Red Ball', 'Blue Cube', 'Blue Triangle', 'Purple Cross', 'Purple Star', 'Other']
+
+class simple_object_detector_node:
+
+    def __init__(self):
+        rospy.init_node('er_simple_object_detector_node', anonymous=True)
+
+        self.box_publisher = rospy.Publisher("object_bounding_boxes", UInt16MultiArray, queue_size=1)
+
+        # Needed to convert Image message to cv2 image type
+        self.bridge = CvBridge()
+
+        # Subscriber listening for the images sent by the camera, and set the callback function
+        self.image_sub = rospy.Subscriber('camera/rgb/image_rect_color', Image, self.detect_object, queue_size=1)
+
+        self.cnn_model = keras.models.load_model('cnn.h5')
+        self.cnn_model._make_predict_function()
+
+    def run(self):
+        # spin() simply keeps python from exiting until this node is stopped
+        rospy.spin()
+
+    def detect_object(self, data):
+        try:
+            img = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            rospy.logerror('error when converting message to cv2 image')
+            rospy.logerror(e)
+
+        rectangles = find_bounding_boxes(img, DEBUG)
+
+
+        # create message which will be sent
+        # format will be a vector of length 4xn, where n is the number of detected bounding boxes
+        # data is ordered like this: x1, y1, w1, h1, x2, y2, w2, h2, ..., xn, yn, wn, hn
+        bounding_box_msg = UInt16MultiArray()
+        for box in rectangles:
+            (x,y,w,h) = box
+            roi = img[y:y+w, x:x+w]
+            # roi_hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+            roi = np.asarray([cv2.resize(roi, (32, 32))], dtype=np.float32) / 255
+            p = self.cnn_model.predict(roi)
+            idx = np.argmax(p)
+
+            if idx == 14:
+                continue
+
+            if True:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(img, LABELS[idx] + '({0})'.format(p[0][idx]),(x, y + h/2), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),2,cv2.LINE_AA)
+                cv2.imshow('image', img)
+                cv2.waitKey(1)
+
+
+            bounding_box_msg.data.append(x)
+            bounding_box_msg.data.append(y)
+            bounding_box_msg.data.append(w)
+            bounding_box_msg.data.append(h)
+
+        self.box_publisher.publish(bounding_box_msg)
+
+        rospy.logdebug('published the following bounding boxes: ' + str(bounding_box_msg))
+
+if __name__ == '__main__':
+    li = simple_object_detector_node()
+    li.run()
--- er_perception/scripts/train_cnn.py
+++ er_perception/scripts/train_cnn.py
@@ -4,6 +4,11 @@ import os
 import cv2
 from os import listdir
 from os.path import isfile, isdir, join
+from keras import layers
+from keras import models
+from keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 # dataset should be in the following format
@@ -29,17 +34,14 @@ inputsize = (32, 32)
 # how much percent of the images will be used for validation
 validation_percentage = 0.2
 
-mode = "SIMPLE_OBJECT_DETECTOR"
+mode = "TRAIN"
 
 # create test / training data by running the whole computer vision pipeline
 if mode == "SIMPLE_OBJECT_DETECTOR":
     # dataset folder assumed to be in same folder as script
     dirname = os.path.dirname(__file__)
 
-    # onlyfiles = [f for f in listdir(mypath) if isfile(join(dirname, f))]
     dataset_path = os.path.join(dirname, 'dataset')
-
-    print(listdir(dataset_path))
 
     class_folder_names = [dir for dir in listdir(dataset_path) if isdir(os.path.join(dataset_path, dir))]
 
@@ -89,4 +91,90 @@ if mode == "SIMPLE_OBJECT_DETECTOR":
 
 
 if mode == "TRAIN":
-    pass
+    def build_model():
+        model = models.Sequential()
+        model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(inputsize[0], inputsize[1], 3)))
+        model.add(layers.MaxPooling2D((2,2)))
+        model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2,2)))
+        model.add(layers.Conv2D(256, (3,3), activation='relu'))
+        model.add(layers.Flatten())
+        model.add(layers.Dense(64, activation='relu'))
+        model.add(layers.Dense(15, activation='softmax'))
+        model.summary()
+        return model
+
+    def load_data():
+        dirname = os.path.dirname(__file__)
+        dataset_path = os.path.join(dirname, 'dataset')
+        class_folder_names = [dir for dir in listdir(dataset_path) if isdir(os.path.join(dataset_path, dir))]
+        class_names = []
+        class_paths = []
+        class_idx = 0
+        current_idx = 0
+        while True:
+            if current_idx >= len(class_folder_names):
+                break
+            if class_folder_names[current_idx].startswith(str(class_idx)+'_'):
+                class_paths.append(os.path.join(dataset_path, class_folder_names[current_idx]))
+                class_names.append(class_folder_names[current_idx][len(str(class_idx)+'_'):])
+                current_idx = 0
+                class_idx +=1
+            else:
+                current_idx += 1
+
+        class_idx = 0
+        count = 0
+        training_data = []
+        training_labels = []
+        for class_path in class_paths:
+            image_dir = os.path.join(class_path, '{0}x{1}'.format(inputsize[0], inputsize[1]))
+            image_files = [file for file in listdir(image_dir) if isfile(os.path.join(image_dir, file))]
+            for image_file in image_files:
+                img = cv2.imread(os.path.join(image_dir, image_file))
+                # img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+                # print(os.path.join(image_dir, image_file))
+                # print(img.shape)
+                training_data.append(np.asarray(img))
+                training_labels.append(to_categorical(class_idx, num_classes=15))
+            class_idx += 1
+
+        # print(training_labels)
+        training_data = np.asarray(training_data, dtype=np.float32) / 255
+        training_labels = np.asarray(training_labels, dtype=np.float32)
+        return (training_data, training_labels)
+
+    model = build_model()
+    (data, labels) = load_data()
+
+    shuffled_indices = np.arange(data.shape[0])
+    np.random.shuffle(shuffled_indices)
+    data = data[shuffled_indices]
+    labels = labels[shuffled_indices]
+    training_data = data[:250]
+    val_data = data[250:]
+    training_labels = labels[:250]
+    val_labels = labels[250:]
+    print(data.shape)
+    print(labels.shape)
+
+    model.compile(optimizer='rmsprop',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    history = model.fit(training_data, training_labels, epochs=200, batch_size=64, validation_data=(val_data, val_labels))
+
+    # test_loss, test_acc = model.evaluate(test_data, test_labels)
+    # print(test_loss, test_acc)
+
+    acc_values = history.history['acc']
+    val_acc_values = history.history['val_acc']
+    epochs = range(1, len(acc_values) + 1)
+    plt.plot(epochs, acc_values, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc_values, 'b', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+    model.save('cnn.h5')
